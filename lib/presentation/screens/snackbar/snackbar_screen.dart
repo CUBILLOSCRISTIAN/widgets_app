60de5c90a3e0ef2b8d58ec15772b8b36ee57ce14
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-06-20 15:21:06 -0500

    feat: Add Snackbars y dialogs screen
    This commit adds the Snackbars y dialogs screen to the application. The screen displays snackbars and dialogs as indicators on the screen. It includes a custom snackbar with a "Hola mundo" message and an "Ok!" action button.
    
    These additions enhance the user experience by providing informative and interactive indicators on the screen.

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -4,8 +4,29 @@ class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackback = SnackBar(
+      content: const Text('Hola mundo '),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {}),
+      duration: const Duration(seconds: 3),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackback);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbards y dialogs'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackbar(context),
+        label: const Text('Mostrar Snackbard'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+    );
   }
 }
