93865adff344b779bffd7b434707d8545d6ebf12
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-06-20 09:05:57 -0500

    feat: Add GoRouter configuration for routing in the app
    The code changes in `app_router.dart` introduce the GoRouter configuration for handling routing in the application. It defines the initial location as `'/'` and sets up routes for the home screen, buttons screen, and cards screen. Each route specifies a path and a builder function to render the corresponding screen.
    
    This commit adds the `app_router.dart` file to the repository, which will be used to configure the routing in the application.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -55,7 +56,7 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
