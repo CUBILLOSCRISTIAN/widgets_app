c170a897012cc260bb8be967c4eba94691e0b0f7
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-06-20 10:14:51 -0500

    feat: Update theme color in main.dart
    The code changes in `main.dart` update the theme color from 3 to 4 in the `AppTheme` class. This change improves the visual appearance of the application by using a different color scheme.

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
-
-    static const String name = 'buttons_screen';
+  static const String name = 'buttons_screen';
 
   const ButtonsScreen({super.key});
 
@@ -12,7 +12,67 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () => context.pop(),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated Disable')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.access_alarm_rounded),
+                label: const Text('Elevated Icon')),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.abc_rounded),
+                label: const Text('Filled Icon')),
+            OutlinedButton(onPressed: () {}, child: const Text('Outline')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.ac_unit_outlined),
+                label: const Text('Outline icon')),
+            TextButton(onPressed: () {}, child: const Text('TextButton')),
+            TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.yard_rounded),
+                label: const Text('TextButton Icon')),
+
+            //TODO:  Custom button
+
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.app_blocking_outlined)),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_blocking_outlined),
+              style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(colors.primary),
+                  iconColor: const WidgetStatePropertyAll(Colors.white)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
