4de1c967c93bf768f8314e86ca82454e6f4ca5cb
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-06-20 15:41:33 -0500

    feat: Add lazy loading for images in SnackbarScreen and Dialogs
    This commit adds lazy loading functionality for images in the SnackbarScreen and Dialogs. By implementing lazy loading, the application improves page load performance by only loading images when they are visible on the screen. This optimization enhances the user experience by reducing the initial load time and improving overall performance.

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -16,11 +17,47 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackback);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estas seguro?'),
+        content: const Text(
+            'Magna dolor eu anim dolor magna et cillum anim in. Dolore est nisi aliquip ipsum do elit officia consequat nisi labore. Minim elit nulla nulla laborum exercitation do commodo excepteur eiusmod dolor fugiat. Dolore dolore irure deserunt consectetur. Elit mollit duis id minim adipisicing aute pariatur nostrud elit voluptate non adipisicing.'),
+        actions: [
+          TextButton(
+              onPressed: () => context.pop(), child: const Text('Cancelar')),
+          FilledButton(
+              onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbards y dialogs'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text(
+                        'Officia incididunt elit officia id esse reprehenderit aute esse ea dolore officia adipisicing sit aliqua.')
+                  ]);
+                },
+                child: const Text('Licencias usadas')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Mostrar dialogos')),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showCustomSnackbar(context),
